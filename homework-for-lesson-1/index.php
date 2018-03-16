38bbc9502b7bce831b80546ec4281e1ba552cb74
Maxim Goldy <spacenear@ya.ru>
2018-03-16 12:42:08 +0300

    commit

--- homework-for-lesson-1/index.php
+++ homework-for-lesson-1/index.php
@@ -5,11 +5,11 @@ include 'variables.php'
 <!doctype html>
 <html lang="en">
 <head>
-  <meta charset="UTF-8">
-  <title><?php echo $index->get_title(); ?></title>
+	<meta charset="UTF-8">
+	<title><?php echo $index->get_title(); ?></title>
 </head>
 <body>
-  <h1><?php echo $index->get_h1(); ?></h1>
-  <h3><?php echo $index->get_year(); ?></h3>
+	<h1><?php echo $index->get_h1(); ?></h1>
+	<h3><?php echo $index->get_year(); ?></h3>
 </body>
 </html>
